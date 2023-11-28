0d05c33cb34bc2c289b60b012152127aceaf2a21
c0b22147f6 <c0b22147f6@edu.teu.ac.jp>
2023-11-28 14:32:38 +0900

    練習３　矢印キーによるこうかとんの移動

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -5,6 +5,12 @@ import pygame as pg
 
 WIDTH, HEIGHT = 1200, 700
 
+delta = {  # 練習３　矢印での操作
+    pg.K_UP: (0, -5),
+    pg.K_DOWN:(0, +5),
+    pg.K_LEFT:(-5, 0),
+    pg.K_RIGHT:(+5, 0)
+}
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -12,14 +18,15 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = 900, 400
     bom_img = pg.Surface((20,20))  # 練習１：透明なSurfaceを作る
     pg.draw.circle(bom_img, (255, 0, 0), (10, 10), 10)  # 練習１：赤い半径１０の円を描く
     bom_img.set_colorkey((0, 0, 0))  
     bom_rct = bom_img.get_rect()  # 練習1
     bom_rct.centerx = random.randint(0, WIDTH)  # 
     bom_rct.centery = random.randint(0, HEIGHT)
-    vx, vy = +5, -5  # 練習２：爆弾の速度
-
+    vx, vy = +5, -5  # 練習２：爆弾の速度    
 
     clock = pg.time.Clock()
     tmr = 0
@@ -27,14 +34,22 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
+        
+        key_lst = pg.key.get_pressed()
+        sum_move = [0,0]
+        for k, tpi in delta.items():
+            if key_lst[k]: # キーが押されたら
+                sum_move[0] += tpi[0]
+                sum_move[1] += tpi[1]
 
         screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, [900, 400])
+        kk_rct.move_ip(sum_move[0], sum_move[1])
+        screen.blit(kk_img, kk_rct)
         bom_rct.move_ip(vx,vy)
         screen.blit(bom_img, bom_rct)
         pg.display.update()
         tmr += 1
-        clock.tick(10)
+        clock.tick(50)
 
 
 if __name__ == "__main__":
