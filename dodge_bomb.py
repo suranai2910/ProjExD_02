1725ea1fbd22a24362a0c0c797850607a2c3214c
c0b22147f6 <c0b22147f6@edu.teu.ac.jp>
2023-11-28 14:51:13 +0900

    練習４　こうかとんと爆弾が画面外に出ない処理

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -12,6 +12,19 @@ delta = {  # 練習３　矢印での操作
     pg.K_RIGHT:(+5, 0)
 }
 
+def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
+    """
+    オブジェクトが画面内or画面外を判定し、真理値タプルを返す関数
+    引数　rct　こうかとんor爆弾SurfaceのRect
+    戻り値：横方向　縦方向判定結果、（画面内：True/画面外：False）
+    """
+    yoko, tate = True, True
+    if rct.left < 0 or WIDTH < rct.right:
+        yoko = False
+    if rct.top < 0 or HEIGHT < rct.bottom:
+        tate = False
+    return (yoko, tate)
+
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
@@ -44,8 +57,15 @@ def main():
 
         screen.blit(bg_img, [0, 0])
         kk_rct.move_ip(sum_move[0], sum_move[1])
+        if check_bound(kk_rct) != (True, True):
+            kk_rct.move_ip(-sum_move[0], -sum_move[1])
         screen.blit(kk_img, kk_rct)
         bom_rct.move_ip(vx,vy)
+        yoko, tate = check_bound(bom_rct)
+        if not yoko:
+            vx *= -1
+        if not tate:
+            vy *= -1
         screen.blit(bom_img, bom_rct)
         pg.display.update()
         tmr += 1
